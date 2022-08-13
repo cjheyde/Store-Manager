0748156dbeefe239ab93f0c17e074b212571e758
cjheyde <carlaheyde@gmail.com>
2022-08-12 21:50:47 -0300

    tests: 100% test coverage productModel.js

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -5,13 +5,80 @@ const connection = require('../../../models/connection');
 
 const productModel = require('../../../models/productModel');
 
-describe('Requisito 01 - Criar Endpoints para listar produtos', () => {
-  describe('testar o caminho /products usando a função getAll', () => {
-    it('testar se ');
-    it('');
+describe('Requisito 01 - lista dos produtos', () => {
+  describe('Lista os produtos do bd - /products - getAll', () => {
+    describe('Caso de sucesso', () => {
+      afterEach(() => {
+        Sinon.restore();
+      })
+      it('retorna um array', async function () {
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getAll();
+        expect(result).to.be.an('array');
+      });
+      it('o array retornado é vazio', async function () {
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getAll();
+        expect(result).to.be.empty;
+      });
+      it('o array retornado é cheio', async function () {
+        const resultExecute = [{ id: 10, name: 'teste de teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getAll();
+        expect(result).to.be.not.empty;
+      });
+      it('o array retornado contém itens do tipo objeto', async function () {
+        const resultExecute = [{ id: 10, name: 'teste de teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getAll();
+        expect(result[0]).to.be.an('object');
+      });
+      it('o array retornado contém as propriedades: "id" e "name"', async function () {
+        const resultExecute = [{ id: 10, name: 'teste de teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getAll();
+        expect(result[0]).to.all.keys('name', 'id')
+      });
+    });
   });
-  describe('testar o caminho /products/:id', () => {
-    it('');
-    it('');
+  describe('Traz os dados do bd de um produto com Id especifico - /products/:id - getById', () => {
+    describe('Caso de sucesso', () => {
+      afterEach(() => {
+        Sinon.restore();
+      })
+      it('retorna um objeto', async function () {
+        const resultExecute = [{ id: 10, name: 'teste de teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getById(10);
+        expect(result).to.be.an('object');
+      });
+      it('o objeto retornado contém as propriedades: "id" e "name"', async function () {
+        const resultExecute = [{ id: 10, name: 'teste de teste' }];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getById();
+        expect(result).to.all.keys('name', 'id')
+      });
+    });
+    describe('Caso não exista o Id', () => {
+      afterEach(() => {
+        Sinon.restore();
+      })
+      it('retorna null', async function () {
+        const resultExecute = [];
+        Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+        const result = await productModel.getById(10);
+        expect(result).to.be.null;
+      });
+    });
   });
 });
