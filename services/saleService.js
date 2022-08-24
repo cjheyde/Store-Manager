ed45d7773aa2232923903f574b8afdf15e5fb681
cjheyde <carlaheyde@gmail.com>
2022-08-24 04:37:26 -0300

    fix: commented req. 6 and 16 - add and edit for sales

--- services/saleService.js
+++ services/saleService.js
@@ -4,7 +4,7 @@ const getAll = async () => saleModel.getAll();
 
 const getById = async (id) => saleModel.getById(id);
 
-const add = async (itemsSold) => saleModel.add(itemsSold);
+// const add = async (itemsSold) => saleModel.add(itemsSold);
 
 const destroy = async (id) => {
   const result = await saleModel.destroy(id);
@@ -12,10 +12,10 @@ const destroy = async (id) => {
   return true;
 };
 
-const edit = async (saleId, itemsUpdated) => saleModel.edit(saleId, itemsUpdated);
+// const edit = async (saleId, itemsUpdated) => saleModel.edit(saleId, itemsUpdated);
 
 // const edit = async (saleId, itemsUpdated) => {
-//   const result = await saleModel.edit({ saleId, itemsUpdated });
+//   const result = await saleModel.edit(saleId, itemsUpdated);
 //   if (result.affectedRows === 0) return false;
 //   return true;
 // };
@@ -23,7 +23,7 @@ const edit = async (saleId, itemsUpdated) => saleModel.edit(saleId, itemsUpdated
 module.exports = {
   getAll,
   getById,
-  add,
   destroy,
-  edit,
+  // add,
+  // edit,
 };
