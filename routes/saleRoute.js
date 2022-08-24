5044c6c79d8b520ec8993f55f214bcba4da1cfb2
cjheyde <carlaheyde@gmail.com>
2022-08-24 04:50:38 -0300

    fix: comennted all related to sales add and edit

--- routes/saleRoute.js
+++ routes/saleRoute.js
@@ -1,17 +1,17 @@
 const express = require('express');
 
 const saleController = require('../controllers/saleController');
-const { validacaoSale } = require('../middlewares/validacaoSale');
+// const { validacaoSale } = require('../middlewares/validacaoSale');
 
 const saleRoute = express.Router();
 
 saleRoute.get('/', saleController.getAll);
 
-saleRoute.post('/', validacaoSale, saleController.add);
+// saleRoute.post('/', validacaoSale, saleController.add);
 
 saleRoute.get('/:id', saleController.getById);
 
-saleRoute.put('/:id', validacaoSale, saleController.edit);
+// saleRoute.put('/:id', validacaoSale, saleController.edit);
 
 saleRoute.delete('/:id', saleController.destroy);
 
